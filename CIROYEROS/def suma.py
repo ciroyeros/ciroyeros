76b6eb2bcb66628891dcd4e19c09c548bac78e11
ciroyeros <carreno.giardina@gmail.com>
2025-03-21 13:23:06 -0300

    Add files via upload

---
+++ CIROYEROS/def suma.py
@@ -0,0 +1,8 @@
+def suma(a,b):
+    resultado=a+b
+    return resultado
+numero1=int(input("introduce el primer numero"))
+numero2=int(input("introduce el segundo numero"))
+
+resultado=suma(numero1,numero2)
+print(resultado)
